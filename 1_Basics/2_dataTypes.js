858130ac58cf85acbc16050ea57d7e537995d4f8
Mayank Dhosriya <114990481+Mayankd31@users.noreply.github.com>
2023-12-16 18:34:21 +0000

    datatypes

---
+++ 1_Basics/2_dataTypes.js
@@ -0,0 +1,29 @@
+"user strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// the readability of the code should be high
+
+let name = "mayank"
+let age = 20
+let isLoggedIn = false
+let a;
+
+
+// primitive data types =>
+// number => 2 to power 53
+// bigint => use for large numbers
+// string => "" or ''
+// boolean => true/false
+// null => standalone value
+// undefined => the value is not assingned to the variable
+// symbol => unique(use for the uniqueness)
+
+
+// object
+
+
+console.log(typeof "mayank"); // string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
