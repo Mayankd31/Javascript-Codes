772d2da4bf53a8880bb143203e2a15a67d7eb19a
Mayank Dhosriya <114990481+Mayankd31@users.noreply.github.com>
2023-12-17 05:30:39 +0000

    conversion of datatypes and operations

--- 1_Basics/3_coversionOperation.js
+++ 1_Basics/3_coversionOperation.js
@@ -59,6 +59,53 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let number = 33
 
 let stringNumber = String(number)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************* Operations *************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(3%2);
+
+
+let str1 = "hello"
+let str2 = " mayank"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
+
+
+
+
 
