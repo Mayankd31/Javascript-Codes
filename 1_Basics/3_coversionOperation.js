57b4a0eccd2e17b6711f3a306e8f3ab6f6c0ec7c
Mayank Dhosriya <114990481+Mayankd31@users.noreply.github.com>
2023-12-17 04:50:54 +0000

    conversion of datatypes

---
+++ 1_Basics/3_coversionOperation.js
@@ -0,0 +1,64 @@
+let score = "33"
+let score2 = "123abc"
+let score3 = null
+let score4 = undefined
+let score5 = true
+let score6 = "mayank"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+// console.log(typeof score2);
+// console.log(typeof score3);
+// console.log(typeof score4);
+// console.log(typeof score5);
+// console.log(typeof score6);
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let valueInNumber1 = Number(score2)
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
+
+// let valueInNumber2 = Number(score3)
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+
+// let valueInNumber3 = Number(score4)
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3);
+
+// let valueInNumber4 = Number(score5)
+// console.log(typeof valueInNumber4);
+// console.log(valueInNumber4);
+
+// let valueInNumber5 = Number(score6)
+// console.log(typeof valueInNumber5);
+// console.log(valueInNumber5);
+
+
+// "33" => 33
+// "123abc" => NaN(Not a number)
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+// "mayank" => NaN
+
+let isLoggedIn = "mayank"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "mayank" => true
+
+
+let number = 33
+
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
