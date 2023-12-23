4fdb0f4aa1b31d30f358e772f94f6b9239efdb35
Mayank Dhosriya <114990481+Mayankd31@users.noreply.github.com>
2023-12-23 03:22:50 +0000

    Math and num

---
+++ 1_Basics/6_nums_and_math.js
@@ -0,0 +1,46 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const number = 112.675757
+// console.log(number.toPrecision(4));
+
+
+const value = 1000000
+// console.log(value.toLocaleString('en-IN'));
+
+
+// ***************** Maths *****************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4,1,6,7,8));
+// console.log(Math.max(4,1,6,7,8));
+
+console.log(Math.random());// gives the value between 0 and 1 it can be 0 and 1 also
+
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1); // we add 1 for avoiding the 0 value
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
