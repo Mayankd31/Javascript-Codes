ad048ac019304a1612f34a1a3b7459eaf0c52d82
Mayank Dhosriya <114990481+Mayankd31@users.noreply.github.com>
2023-12-23 19:02:09 +0000

    Math updated

--- 1_Basics/6_nums_and_math.js
+++ 1_Basics/6_nums_and_math.js
@@ -1,12 +1,13 @@
 const score = 400
 // console.log(score);
 
-const balance = new Number(100)
+const balance = new Number(100.987)
 // console.log(balance);
 
 
 // console.log(balance.toString().length);
 // console.log(balance.toFixed(2));
+
 
 
 const number = 112.675757
@@ -26,11 +27,16 @@ const value = 1000000
 // console.log(Math.floor(4.6));
 // console.log(Math.min(4,1,6,7,8));
 // console.log(Math.max(4,1,6,7,8));
+console.log(Math.PI);
+console.log(Math.trunc(4.567)); // returns the integer value
+console.log(Math.pow(8,2)); // 2 ki power 8 => 64
+console.log(Math.sqrt(64)); // 8
+
 
 console.log(Math.random());// gives the value between 0 and 1 it can be 0 and 1 also
 
 console.log((Math.random()*10) + 1);
-console.log((Math.random()*10) + 1); // we add 1 for avoiding the 0 value
+console.log(Math.floor((Math.random()*10)) + 1); // we add 1 for avoiding the 0 value // 1 to 10
 
 
 const min = 10
